d8dd00aa87657c8bad871fdf6da0622ec26276e7
Denilson Mucavele <denilsonmucavel@outlook.com>
2021-11-28 22:28:27 -0800

    changing app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,10 +13,8 @@ import { ComponentsModule } from './components/components.module';
     debug: process.env.NODE_ENV === "development" ? true : false,
     autoSchemaFile:true,
   }),
-
   ComponentsModule,  
   ],
-  
   controllers: [AppController],
   providers: [AppService],
 })
